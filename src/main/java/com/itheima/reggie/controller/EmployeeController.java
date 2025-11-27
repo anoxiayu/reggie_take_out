60af7ba2e80d0f5b84db79877b5f7aa93cc7748c
anoxiayu <247801313@qq.com>
2025-11-27 22:43:57 +0800

    Initial commit

---
+++ src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -0,0 +1,60 @@
+package com.itheima.reggie.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.entity.Employee;
+import com.itheima.reggie.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @PostMapping("/login")
+    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee){
+        //MD5加密处理密码
+        //
+        String password = employee.getPassword();
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
+
+        //
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Employee::getUsername, employee.getUsername());
+        Employee emp = employeeService.getOne(queryWrapper);
+
+        if(emp == null){
+            return R.error("登录失败");
+        }
+
+        if(!password.equals(emp.getPassword())){
+            return R.error("登录失败");
+        }
+
+        if(emp.getStatus() == 0){
+            return R.error("账号禁用");
+        }
+
+        request.getSession().setAttribute("employee", emp.getId());
+        return R.success(emp);
+    }
+
+    @PostMapping("/logout")
+    public R<String> logout(HttpServletRequest request){
+        request.getSession().removeAttribute("employee");
+        return R.success("退出成功");
+    }
+
+
+}
---
+++ src/main/java/com/itheima/reggie/filter/LoginCheckFilter.java
@@ -0,0 +1,72 @@
+package com.itheima.reggie.filter;
+
+import com.alibaba.fastjson.JSON;
+import com.itheima.reggie.common.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.AntPathMatcher;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//检查是否完成登录
+@WebFilter(filterName = "loginCheckFilter",urlPatterns = "/*")
+@Slf4j
+public class LoginCheckFilter implements Filter {
+    //路径匹配器，支持通配符
+    public static final AntPathMatcher ANT_PATH_MATCHER = new AntPathMatcher();
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        //1.获取url
+        //定义不需要处理的url
+        //2.判断是否需要处理
+        //3.如果不需要处理
+        //4.判断登录状态
+        //5.如果未登录则返回未登录结果,通过输出流方式向客户端页面相应数据
+
+
+
+        String requestURI = request.getRequestURI();
+
+
+        String[] urls = new String[]{
+                "/employee/login",
+                "/employee/logout",
+                "/backend/**",
+                "/front/**"
+        };
+
+        boolean check = check(urls,requestURI);
+
+
+        if(check){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        if(request.getSession().getAttribute("employee") != null){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+
+        response.getWriter().write(JSON.toJSONString(R.error("NOTLOGIN")));
+        return;
+    }
+
+    public boolean check(String[] urls,String requestURI){
+        for (String url:urls){
+            if(ANT_PATH_MATCHER.match(url,requestURI)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
