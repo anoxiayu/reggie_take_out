a8fd5f75686e8deb4a58637faa8e9619a1b3100a
anoxiayu <247801313@qq.com>
2025-12-02 19:20:03 +0800

    1201

---
+++ src/main/java/com/itheima/reggie/controller/SetmealController.java
@@ -0,0 +1,92 @@
+package com.itheima.reggie.controller;
+
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.dto.DishDto;
+import com.itheima.reggie.dto.SetmealDto;
+import com.itheima.reggie.entity.Category;
+import com.itheima.reggie.entity.Dish;
+import com.itheima.reggie.entity.Setmeal;
+import com.itheima.reggie.service.CategoryService;
+import com.itheima.reggie.service.SetmealDishService;
+import com.itheima.reggie.service.SetmealService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+/**
+ * 套餐管理
+ */
+
+@RestController
+@Slf4j
+@RequestMapping("/setmeal")
+public class SetmealController {
+
+    @Autowired
+    SetmealDishService setmealDishService;
+
+    @Autowired
+    SetmealService setmealService;
+
+    @Autowired
+    CategoryService categoryService;
+
+    /**
+     * 新增套餐
+     * @param setmealDto
+     * @return
+     */
+    @PostMapping()
+    public R<String> save(@RequestBody SetmealDto setmealDto) {
+        log.info("setmealDto={}", setmealDto.toString());
+        setmealService.saveWithDish(setmealDto);
+        return R.success("添加套餐成功");
+    }
+
+    @GetMapping("/page")
+    public R<Page> page(int page,int pageSize,String name){
+        //分页构造器对象
+        Page<Setmeal> pageInfo = new Page<>(page,pageSize);
+        Page<SetmealDto> pageDto = new Page<>(page,pageSize);
+        LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.like(name != null,Setmeal::getName,name);
+
+        queryWrapper.orderByDesc(Setmeal::getUpdateTime);
+        setmealService.page(pageInfo,queryWrapper);
+
+        BeanUtils.copyProperties(pageInfo,pageDto,"records");
+
+        List<Setmeal> records = pageInfo.getRecords();
+        List<SetmealDto> list = records.stream().map((item)->{
+
+            SetmealDto setmealDto = new SetmealDto();
+            BeanUtils.copyProperties(item,setmealDto);
+            Long categoryId = item.getCategoryId();
+            Category category = categoryService.getById(categoryId);
+            if(category!=null){
+                String categoryName = category.getName();
+                setmealDto.setCategoryName(categoryName);
+            }
+
+            return setmealDto;
+        }).collect(Collectors.toList());
+
+        pageDto.setRecords(list);
+
+        return R.success(pageInfo);
+    }
+
+    @DeleteMapping
+    public R<String> delete(@RequestParam List<Long> ids){
+        setmealService.deleteWithDish(ids);
+        return R.success("删除套餐成功");
+    }
+}
--- src/main/java/com/itheima/reggie/service/impl/SetmealServiceImlp.java
+++ src/main/java/com/itheima/reggie/service/impl/SetmealServiceImlp.java
@@ -1,13 +1,62 @@
 package com.itheima.reggie.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.itheima.reggie.common.CustomException;
+import com.itheima.reggie.dto.SetmealDto;
+import com.itheima.reggie.entity.Dish;
+import com.itheima.reggie.entity.DishFlavor;
 import com.itheima.reggie.entity.Setmeal;
+import com.itheima.reggie.entity.SetmealDish;
 import com.itheima.reggie.mapper.SetmealMapper;
+import com.itheima.reggie.service.SetmealDishService;
 import com.itheima.reggie.service.SetmealService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 public class SetmealServiceImlp extends ServiceImpl<SetmealMapper, Setmeal> implements SetmealService {
+
+    @Autowired
+    private SetmealDishService setmealDishService;
+
+    @Override
+    @Transactional
+    public void saveWithDish(SetmealDto setmealDto) {
+        //保存菜品基本信息 操作setmeal,执行insert操作 -> 保存套餐和菜品的关联信息,操作setmeal_dish,执行insert操作
+        this.save(setmealDto);
+
+        List<SetmealDish> setmealDishes = setmealDto.getSetmealDishes();
+        setmealDishes = setmealDishes.stream().map((d) -> {
+            d.setSetmealId(setmealDto.getId());
+            return d;
+        }).collect(Collectors.toList());
+
+        setmealDishService.saveBatch(setmealDishes);
+    }
+
+    @Override
+    @Transactional
+    public void deleteWithDish(List<Long> ids) {
+        //查询套餐状态,确定是否可以删除 -> 如果不能删除,抛出业务异常 ->可以删除的话 先删除套餐表中的数据 ->删除关系表中的数据
+        LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.in(Setmeal::getId, ids);
+        queryWrapper.eq(Setmeal::getStatus,1);
+        int count = this.count(queryWrapper);
+        if(count>0){
+            throw new CustomException("套餐售卖中,不能删除");
+        }
+        this.removeByIds(ids);
+
+        LambdaQueryWrapper<SetmealDish> queryWrapper2 = new LambdaQueryWrapper<>();
+        queryWrapper2.in(SetmealDish::getSetmealId, ids);
+        setmealDishService.remove(queryWrapper2);
+
+    }
 }
